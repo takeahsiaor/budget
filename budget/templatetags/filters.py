82dda2ca533c2e78abb77dc87f9e3a58d378edce
takeahsiaor <rhsiao2@gmail.com>
2015-08-08 00:11:05 -0500

    Adds tab index to transaction form

--- budget/templatetags/filters.py
+++ budget/templatetags/filters.py
@@ -36,4 +36,13 @@ register = template.Library()
 #     return spent
 
 
-# register.filter('amount_left_in_category', amount_left_in_category)
+
+@register.filter
+def tabindex(value, index):
+    """
+    Add a tabindex attribute to the widget for a bound field.
+    """
+    value.field.widget.attrs['tabindex'] = index
+    return value
+
+register.filter('tabindex', tabindex)
